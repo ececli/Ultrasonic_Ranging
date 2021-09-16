fc0a47f53d2260d6755ec6c979730018d3c7fb30
ececli <changli.nist@gmail.com>
2021-09-16 18:03:44 -0400

    debug

--- Impl_Pico_Lib.py
+++ Impl_Pico_Lib.py
@@ -1,9 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
 def sendSignal(PIN,Duration):
     GPIO.output(PIN,True)
     time.sleep(Duration)
     GPIO.output(PIN,False)
     return
-
 
 def calAbsSampleIndex(counter, Index, CHUNK, NumIgnoredFrame = 0, NumReqFrames=0):
     return (counter-NumIgnoredFrame - NumReqFrames)*CHUNK + Index
