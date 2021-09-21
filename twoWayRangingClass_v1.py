128f8304038e7bc6ebb9ca781b4eaa5cbe2184ad
ececli <changli.nist@gmail.com>
2021-09-21 18:01:12 -0400

    debug

--- twoWayRangingClass_v1.py
+++ twoWayRangingClass_v1.py
@@ -308,7 +308,7 @@ class TWR:
     
     
     def procTX_ResponderAdditional(self):
-        if Flag_T_RX_Ready:
+        if self.Flag_T_RX_Ready:
             T3T2 = self.T_TX - self.T_RX
             self.T3T2_Record.append(T3T2)
             self.mqttc.sendMsg(self.topic_t3_t2, T3T2)
