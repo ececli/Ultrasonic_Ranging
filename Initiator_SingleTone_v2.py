06841d0c0975b2824339b00ab090ea9c8fb7b2db
ececli <changli.nist@gmail.com>
2021-09-21 21:14:31 -0400

    debug

--- Initiator_SingleTone_v2.py
+++ Initiator_SingleTone_v2.py
@@ -1,16 +1,20 @@
 from twoWayRangingClass_v1 import TWR
 import matplotlib.pyplot as plt
 import evaluationLib_v1 as evalu
+import time
 
 
 initiator = TWR('initiator','UR_pyConfig_v2.conf')
 initiator.initialize()
+startTime = time.time()
 initiator.start()
+duration = time.time() - startTime
+print(duration)
 
 
-plt.figure()
-plt.plot(initiator.Ranging_Record,'b.')
-plt.grid()
-plt.show()
+# plt.figure()
+# plt.plot(initiator.Ranging_Record,'b.')
+# plt.grid()
+# plt.show()
 
 evalu.errorStat(initiator.Ranging_Record, GT = 0.53)
