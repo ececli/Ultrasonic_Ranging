a92a3d74e417bfdc9b821b0e8cc69f869c258e4c
ececli <changli.nist@gmail.com>
2021-09-30 23:35:28 -0400

    add Class_v3

--- twoWayRangingClass_v3.py
+++ twoWayRangingClass_v3.py
@@ -358,14 +358,14 @@ class TWR:
 ############################################################################
     def sendT3T2(self):
         self.init_communications()
-        # time.sleep(0.5)
+        time.sleep(0.5)
 
         self.mqttc.sendMsg(self.topic_t3t2, self.T3T2_Record)
         print("Sending T3-T2 Status: Done")
         
     def recvT3T2(self):
         self.init_communications()
-        # time.sleep(0.5)
+        time.sleep(0.5)
         while True:
             if self.mqttc.checkTopicDataLength(self.topic_t3t2)>=self.NumRanging:
                 break
