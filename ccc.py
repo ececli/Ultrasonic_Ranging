aa2188f14407db0766bc7e70e10e7f50514f5574
ececli <changli.nist@gmail.com>
2022-01-04 15:25:23 -0500

    test zmq in two devices

---
+++ ccc.py
@@ -0,0 +1,49 @@
+import zmq
+import numpy as np
+import time
+
+
+dt = np.dtype([('counter', 'i4'),
+               ('status', 'i4'),
+               ('timestamp', 'f8')
+               ])
+
+
+# initialize ZMQ
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("ipc:///dev/shm/mic_data")
+socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+# context2 = zmq.Context()
+# publisher = context2.socket(zmq.PUB)
+server = context.socket(zmq.REP)
+server.bind("tcp://*:5556")
+
+counter = 0
+while True:
+    rawData = socket.recv()
+    counter = counter + 1
+    header = np.frombuffer(rawData[:16], dtype=dt)
+    mic_data = np.frombuffer(rawData[16:], dtype=np.int32)
+    mic = mic_data >>14
+    COUNT = header[0][0]
+    status = header[0][1]
+    TS = header[0][2] # no use so far
+    print(COUNT,mic)
+    if counter == 10:
+        break
+
+
+a = np.int32(np.floor(np.random.rand(10)*10000))
+print(a)
+
+time.sleep(5)
+while True:
+    message = server.recv()
+    print("Received Request: ",message)
+    time.sleep(1)
+    server.send_pyobj(a)
+
+# publisher.send_multipart([b"B", b"We would like to see this"])
+
---
+++ ddd.py
@@ -0,0 +1,46 @@
+import zmq
+import numpy as np
+import time
+
+
+dt = np.dtype([('counter', 'i4'),
+               ('status', 'i4'),
+               ('timestamp', 'f8')
+               ])
+
+
+# initialize ZMQ
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("ipc:///dev/shm/mic_data")
+socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+
+
+counter = 0
+while True:
+    rawData = socket.recv()
+    counter = counter + 1
+    header = np.frombuffer(rawData[:16], dtype=dt)
+    mic_data = np.frombuffer(rawData[16:], dtype=np.int32)
+    mic = mic_data >>14
+    COUNT = header[0][0]
+    status = header[0][1]
+    TS = header[0][2] # no use so far
+    print(COUNT,mic)
+    if counter == 10:
+        break
+
+
+
+
+
+
+# Prepare our context and publisher
+string = "tcp://192.168.68.130:5556"
+client = context.socket(zmq.REQ)
+client.connect(string)
+
+client.send("Chang")
+a = subscriber.recv_pyobj()
+print(a)
