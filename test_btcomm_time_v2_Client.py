d14d7893664eab4d96ce9fc5c4ec02040c632313
ececli <changli.nist@gmail.com>
2022-02-16 10:01:59 -0500

    debug

---
+++ test_btcomm_time_v2_Client.py
@@ -0,0 +1,65 @@
+import zmq
+import bluetooth
+import time
+
+
+T3T2 = 2234.0
+
+address_list = ['DC:A6:32:E1:9F:C8', 'DC:A6:32:E8:BF:E0']
+
+own_address = bluetooth.read_local_bdaddr()[0]
+
+for addr in address_list:
+    if own_address.upper() != addr.upper():
+        target_address = addr
+
+print("Own Address is ", own_address)
+print("Target Address is ",target_address)
+
+
+
+port = 1
+
+
+
+# bt_sock_T=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+bt_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+
+time.sleep(5)
+# bt_sock_T.connect((target_address, port2))
+bt_sock.connect((target_address, port))
+print("Connected to the other device.")
+time.sleep(1)
+
+startTime = time.time()
+counter = 0
+try:
+    while True:
+
+            
+            # bt_sock_T.send(str(T3T2).encode())
+        bt_sock.send(str(counter).encode())
+        Received = bt_sock.recv(255).decode() 
+        counter = counter + 1
+
+        if counter == 1000:
+            duration = time.time() - startTime
+            break
+
+
+
+
+
+
+except Exception as e: 
+    print(e)
+    bt_sock.close()
+    print('Error! Disconnect!')
+except KeyboardInterrupt:
+
+    bt_sock.close()
+    print('Disconnect by user.')   
+print("Duration is ", duration)
+bt_sock.close()
+
---
+++ test_btcomm_time_v2_Server.py
@@ -0,0 +1,61 @@
+import zmq
+import bluetooth
+import time
+
+
+T3T2 = 2234.0
+
+address_list = ['DC:A6:32:E1:9F:C8', 'DC:A6:32:E8:BF:E0']
+
+own_address = bluetooth.read_local_bdaddr()[0]
+
+for addr in address_list:
+    if own_address.upper() != addr.upper():
+        target_address = addr
+
+print("Own Address is ", own_address)
+print("Target Address is ",target_address)
+
+port = 1
+
+
+# bt_sock_T=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+bt_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+
+bt_sock.bind(("", port))   
+bt_sock.listen(1)
+print("Binded to own device with port %d." % port)
+
+
+try:
+    while True:
+        print('Waiting data from the other device')
+        client_sock, address = bt_sock.accept()  
+        print("Accepted connection from ", address)
+
+
+        counter = 0
+        while True:
+            T3T2 = client_sock.recv(255).decode() 
+            print("received T3T2: %s at %d" % (T3T2,counter))
+            # bt_sock_T.send(str(T3T2).encode())
+            # bt_sock.send(str(T3T2).encode())
+            client_sock.send(str(T3T2).encode())
+            counter = counter + 1
+
+
+
+except Exception as e: 
+    print(e)
+    client_sock.close()
+    bt_sock.close()
+
+    print('Error! Disconnect!')
+except KeyboardInterrupt:
+    client_sock.close()
+    bt_sock.close(
+    print('Disconnect by user.')   
+
+
+
