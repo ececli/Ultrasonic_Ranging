f0addf0b5147d18fc9cffa66fe80cb7cb1b4c078
ececli <changli.nist@gmail.com>
2022-02-23 13:47:11 -0500

    test bluetooth

---
+++ test_btcomm_time_v3_Client.py
@@ -0,0 +1,81 @@
+import zmq
+import bluetooth
+import time
+import numpy as np
+
+
+dt_bt = np.dtype([('Flag_Valid','?'),
+                ('Flag_NumSamples', '?'),
+                ('Flag_LastReq','?'),
+                ('NumSamples', 'i4'),
+                ('NumReTransmission', "i4")
+                ])
+
+bt_data = np.rec.array(np.zeros(1, dtype=dt_bt))
+
+address_list = ['DC:A6:32:E1:9F:C8', 'DC:A6:32:E8:BF:E0']
+
+own_address = bluetooth.read_local_bdaddr()[0]
+
+for addr in address_list:
+    if own_address.upper() != addr.upper():
+        target_address = addr
+
+print("Own Address is ", own_address)
+print("Target Address is ",target_address)
+
+
+
+port = 0x1001
+
+
+
+# bt_sock_T=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+bt_sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+
+time.sleep(5)
+# bt_sock_T.connect((target_address, port2))
+bt_sock.connect((target_address, port))
+print("Connected to the other device.")
+time.sleep(1)
+counter = 0
+startTime = time.time()
+
+try:
+    while True:
+        bt_data.Flag_NumSamples = True
+        bt_data.Flag_Valid = True 
+        bt_data.NumSamples = counter
+        bt_data.NumReTransmission = 0
+        bt_data.Flag_LastReq = False
+        raw_bt_data = bt_data.tobytes()
+        bt_sock.send(raw_bt_data)
+        # print("Sent ",counter)
+        Received = bt_sock.recv(255)
+        # print(Received)
+        # bt_data_R = np.frombuffer(Received, dtype=dt_bt)
+        # print(Received)
+        counter = counter + 1
+        # time.sleep(0.5)
+
+        if counter == 1000:
+            duration = time.time() - startTime
+            break
+
+
+
+
+
+
+except Exception as e: 
+    print(e)
+    bt_sock.close()
+    print('Error! Disconnect!')
+except KeyboardInterrupt:
+
+    bt_sock.close()
+    print('Disconnect by user.')   
+print("Duration is ", duration)
+bt_sock.close()
+
---
+++ test_btcomm_time_v3_Server.py
@@ -0,0 +1,76 @@
+import zmq
+import bluetooth
+import time
+import numpy as np
+
+'''
+dt_bt = np.dtype([('id','U17'),
+                ('status', 'i4'),
+                ('NumSamples', 'i4')
+                ])
+'''
+dt_bt = np.dtype([('Flag_Valid','?'),
+                ('Flag_NumSamples', '?'),
+                ('Flag_LastReq','?'),
+                ('NumSamples', 'i4'),
+                ('NumReTransmission', "i4")
+                ])
+
+
+address_list = ['DC:A6:32:E1:9F:C8', 'DC:A6:32:E8:BF:E0']
+
+own_address = bluetooth.read_local_bdaddr()[0]
+
+for addr in address_list:
+    if own_address.upper() != addr.upper():
+        target_address = addr
+
+print("Own Address is ", own_address)
+print("Target Address is ",target_address)
+
+port = 0x1001
+
+
+# bt_sock_T=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+bt_sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+
+bt_sock.bind(("", port))   
+bt_sock.listen(1)
+print("Binded to own device with port %d." % port)
+
+
+try:
+    while True:
+        print('Waiting data from the other device')
+        client_sock, address = bt_sock.accept()  
+        print("Accepted connection from ", address)
+
+
+        counter = 0
+        while True:
+            # T3T2 = client_sock.recv(255).decode() 
+            raw_bt_data = client_sock.recv(255)
+            bt_data = np.frombuffer(raw_bt_data, dtype=dt_bt)
+            print(counter)
+            print("length of data is ", len(bt_data))
+            print(bt_data)
+            print(bt_data[-1][3])
+            print("received T3T2: %s at %d" % (T3T2,counter))
+            T3T2 = bt_data[-1][3]
+            client_sock.send(str(T3T2).encode())
+            counter = counter + 1
+            # time.sleep(1)
+
+
+
+except Exception as e: 
+    print(e)
+    client_sock.close()
+    bt_sock.close()
+
+    print('Error! Disconnect!')
+except KeyboardInterrupt:
+    client_sock.close()
+    bt_sock.close()
+    print('Disconnect by user.')
