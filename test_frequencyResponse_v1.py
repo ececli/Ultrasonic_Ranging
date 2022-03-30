2922a9302b1454965846f8baaa37c806232e581b
ececli <chang.nist@gmail.com>
2022-03-30 15:58:26 -0400

    test freq resp

---
+++ test_frequencyResponse_v1.py
@@ -0,0 +1,27 @@
+import pigpio
+import time
+
+ratio = 500000
+pin_OUT = 12
+
+f0 = 30000
+plus = -500
+stop_f0 = 20000
+duration = 10
+
+pi_IO = pigpio.pi()
+pi_IO.set_mode(pin_OUT,pigpio.OUTPUT)
+pi_IO.hardware_PWM(pin_OUT,0,0)
+
+
+while True:
+    pi_IO.hardware_PWM(pin_OUT,f0,ratio)
+
+    startTS = time.time()
+    while (time.time() - startTS) < duration:
+        pass
+    f0 = f0 + plus
+    if f0 < stop_f0:
+        break
+
+pi_IO.hardware_PWM(pin_OUT,0,0)
---
+++ test_frequencyResponse_v2.py
@@ -0,0 +1,25 @@
+import pigpio
+import time
+
+ratio = 500000
+pin_OUT = 12
+
+f0 = 22000
+
+
+pi_IO = pigpio.pi()
+pi_IO.set_mode(pin_OUT,pigpio.OUTPUT)
+pi_IO.hardware_PWM(pin_OUT,0,0)
+
+
+try:
+    
+    pi_IO.hardware_PWM(pin_OUT,f0,ratio)
+    print("Start sending ultrasound at frequency ",f0)
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Terminated by user")
+
+pi_IO.hardware_PWM(pin_OUT,0,0)
+
